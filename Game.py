0e8b0a606e2c0a1a29d7d970bea69e60a1ec604d
lucassett <lucassettergren@gmail.com>
2023-04-12 20:32:24 -0700

    documentation

--- Game.py
+++ Game.py
@@ -5,9 +5,17 @@ from Player import *
 
 class Game:
     def __init__(self):
+        '''
+        A game has 4 players - it tracks the pieces each player has placed
+        and rotates between the players.
+        '''
         self.player = [Player(RED), Player(BLU), Player(YLW), Player(GRN)]
 
     def rotate_player(self):
+        '''
+        Rotate from active player to next player in order
+        :return:
+        '''
         return False
 
 

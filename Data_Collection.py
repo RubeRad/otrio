f2224c3fea015ced61e895c45e77a306b3203270
lucassett <lucassettergren@gmail.com>
2023-05-02 12:19:31 -0700

    Function to return game results + runtime

--- Data_Collection.py
+++ Data_Collection.py
@@ -1,7 +1,10 @@
 import numpy as np
 import random
+import time
 
-from Game import *          # Methods to play game
+from OtrioArtist import *
+from Player import *
+from Game import *
 
 #This script is to collect information from an arbitrary number of games (2.7m???).
 
@@ -12,3 +15,23 @@ from Game import *          # Methods to play game
     #Images of final game states (and one move before???)
 
 #File formats include: .csv, .png
+
+def record_game_results(number):
+    start_time = time.time()
+    result_count = [0] * 5
+    result = []
+
+    for i in range(number):
+        g = Game()
+        result.append(g.play_game())
+
+    for r in result:
+        result_count[r] += 1
+
+    end_time = time.time()
+    run_time = end_time - start_time
+    return result_count, run_time
+
+game_results, run_time = record_game_results(10000)
+print(game_results, float(run_time))
+

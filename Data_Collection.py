58a7383481341881313e6dafdec03d4c383bcc50
lucassett <lucassettergren@gmail.com>
2023-05-03 16:01:54 -0700

    Recording first moves and aggregate results for P0

--- Data_Collection.py
+++ Data_Collection.py
@@ -37,15 +37,49 @@ def record_game_results(number):
     run_time = end_time - start_time
     return str_result_count, run_time, result_count
 
-str_game_res, run_time, int_game_res = record_game_results(10000)
+def p0_start_game_results(number):
+    start_time = time.time()
+    result_count = [0] * 5
+    str_result_count = [0] * 5
+    p0_win = 0
+    p0_loss = 0
+    p0_tie = 0
+    start_index = []
 
-headers = ['p0', 'p1', 'p2', 'p3', 'ties']
-with open('Otrio_Stats_TEST.csv', 'w') as f:
-    f.write(','.join(headers))
-    f.write('\n')
-    f.write(','.join(str_game_res))
-    f.close()
+    for i in range(number):
+        g = Game()
+        firstmove = g.single_move(0)
+        start_index.append(firstmove)
+        winner = g.play_game(next_player=1)
 
-print(int_game_res, float(run_time))
+        if winner == 0:
+            p0_win += 1
+        elif winner == 4:
+            p0_tie += 1
+        else:
+            p0_loss += 1
+
+    p0_results = list(str(p0_win))
+    p0_results.append(str(p0_loss))
+    p0_results.append(str(p0_tie))
+
+    return start_index, p0_results
+    #print(p0_results)
+
+firstmove, outcome = p0_start_game_results(10)
+print(firstmove, outcome)
+
+
+
+#str_game_res, run_time, int_game_res = record_game_results(10000)
+
+#headers = ['p0', 'p1', 'p2', 'p3', 'ties']
+#with open('Otrio_Stats_TEST.csv', 'w') as f:
+    #f.write(','.join(headers))
+    #f.write('\n')
+    #f.write(','.join(str_game_res))
+    #f.close()
+
+#print(int_game_res, float(run_time))
 
 
